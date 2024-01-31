a1c7fd614e6b6e0264609d9c94ebd63c8225359a
Timur <tim5521@yandex.ru>
2024-01-31 15:53:24 +0300

    Task_1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+
+Random rnd = new();
+char[,] ch = new char[3, 3];
+string str = "";
+for (int i = 0; i < ch.GetLongLength(0); i++)
+{
+    for (int j = 0; j < ch.GetLength(1); j++)
+    {
+        ch[i, j] = (char)('a' + rnd.Next(0, 26));
+    }
+}
+for (int i = 0; i < ch.GetLongLength(0); i++)
+{
+    for (int j = 0; j < ch.GetLength(1); j++)
+    {
+        str +=ch[i,j];
+    }
+}
+Console.WriteLine(str);
