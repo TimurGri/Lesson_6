bcc41508228ac3d4caf61e9e84e1180b17ce4759
Timur <tim5521@yandex.ru>
2024-01-31 17:08:31 +0300

    Task_3

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿//Задайте произвольную строку. Выясните, является ли она палиндромом.
+using System;
+
+
+class Program
+{
+    static bool IsPalindrome(string s)
+    {
+        s = s.ToLower();
+        s = s.Replace(" ", "").Replace(",", "").Replace(".", "").Replace("!", "").Replace("?", "");
+        int n = s.Length;
+        for (int i = 0; i < n / 2; i++)
+        {
+            if (s[i] != s[n - i - 1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    static void Main(string[] args)
+    {
+        string s = "Аргентина манит негра";
+        bool result = IsPalindrome(s);
+        Console.WriteLine($"Строка \"{s}\" {(result ? "является" : "не является")} палиндромом.");
+    }
+}
