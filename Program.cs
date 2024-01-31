da7bed9aa52f1e500259263cd542a66361f045b2
Timur <tim5521@yandex.ru>
2024-01-31 16:48:07 +0300

    Task_1 function

--- Program.cs
+++ Program.cs
@@ -1,20 +1,59 @@
 ﻿//Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
 
-Random rnd = new();
-char[,] ch = new char[3, 3];
-string str = "";
-for (int i = 0; i < ch.GetLongLength(0); i++)
+internal class Program
 {
-    for (int j = 0; j < ch.GetLength(1); j++)
+    private static void Main(string[] args)
     {
-        ch[i, j] = (char)('a' + rnd.Next(0, 26));
+
+        char[,] GetRandom2DlowerCaseArray(int row, int col)
+        {
+            Random rnd = new();
+            char[,] ch = new char[row, col];
+            for (int i = 0; i < ch.GetLongLength(0); i++)
+            {
+                for (int j = 0; j < ch.GetLength(1); j++)
+                {
+                    ch[i, j] = (char)('a' + rnd.Next(0, 26));
+                }
+            }
+            return ch;
+        }
+
+        void Print2DcharArray(char[,] ch)
+        {
+            for (int i = 0; i < ch.GetLongLength(0); i++)
+            {
+                for (int j = 0; j < ch.GetLength(1); j++)
+                {
+                    Console.Write(ch[i, j]);
+                    if (j < ch.GetLength(1) - 1) Console.Write(", ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        string Convert2DcharArrayToString(char[,] ch)
+        {
+            string str = "";
+            for (int i = 0; i < ch.GetLength(0); i++)
+            {
+                for (int j = 0; j < ch.GetLength(1); j++)
+                {
+                    str += ch[i, j];
+                }
+            }
+            return str;
+        }
+
+        Console.Write("Input the number of rows of char array: ");
+        int row = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Input the number of cols of char array: ");
+        int col = Convert.ToInt32(Console.ReadLine());
+        char[,] ch = GetRandom2DlowerCaseArray(row, col);
+        string str = Convert2DcharArrayToString(ch);
+        Print2DcharArray(ch);
+        Console.WriteLine(str);
+
+
     }
 }
-for (int i = 0; i < ch.GetLongLength(0); i++)
-{
-    for (int j = 0; j < ch.GetLength(1); j++)
-    {
-        str +=ch[i,j];
-    }
-}
-Console.WriteLine(str);
