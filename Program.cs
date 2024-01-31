fd9909664794cb3ec370e783703a61e9a744fc93
Timur <tim5521@yandex.ru>
2024-01-31 16:11:51 +0300

    Task_2

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿//Задайте строку, содержащую латинские буквы в обоих регистрах. Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+string  str = Console.ReadLine();
+string new_str = str.ToLower();
+Console.WriteLine(new_str);
